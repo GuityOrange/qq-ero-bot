b83e3dee32aa41ea873b4795fc546b7e818710f1
jenkins <liyongqi@cheeselove.cn>
2023-08-12 12:45:14 +0800

    feature:添加简单gpt接入

--- modules/chatgpt.py
+++ modules/chatgpt.py
@@ -7,16 +7,26 @@ from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.model import Group
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import Plain
+from revChatGPT.V1 import Chatbot
+from persistence.gpt_limit import is_under_limit
 
 bcc = create(Broadcast)
 LIMIT = 50
+response = ''
+
+
+def get_response(chatbot, prompt):
+    global response
+    for data in chatbot.ask(
+            prompt
+    ):
+        response = data["message"]
 
 
 @bcc.receiver(GroupMessage)
 async def gpt(app: Ariadne, group: Group, message: MessageChain):
     if re.search(r'gpt(.*)', message.display):
-        from revChatGPT.V1 import Chatbot
-        from persistance.gpt_limit import is_under_limit
+        # 输入内容为空
         if re.search(r'gpt(.*)', message.display).group(1) == '':
             await app.send_message(
                 group,
@@ -24,6 +34,7 @@ async def gpt(app: Ariadne, group: Group, message: MessageChain):
             )
             return
 
+        # 本小时次数已用完
         reachable, used_count = is_under_limit(LIMIT)
         if not reachable:
             await app.send_message(
@@ -32,19 +43,16 @@ async def gpt(app: Ariadne, group: Group, message: MessageChain):
             )
             return
 
+        # 本小时次数未用完,走正常流程
         chatbot = Chatbot(config={
             "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiI1NzQzMDYzOTRAcXEuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9LCJodHRwczovL2FwaS5vcGVuYWkuY29tL2F1dGgiOnsidXNlcl9pZCI6InVzZXItMkVTazRWUEhCa213c3hSWWRMejU5TE9PIn0sImlzcyI6Imh0dHBzOi8vYXV0aDAub3BlbmFpLmNvbS8iLCJzdWIiOiJhdXRoMHw2MzhlMGJjNGRhYTJjMzQwMGViNzI2NWQiLCJhdWQiOlsiaHR0cHM6Ly9hcGkub3BlbmFpLmNvbS92MSIsImh0dHBzOi8vb3BlbmFpLm9wZW5haS5hdXRoMGFwcC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjkxODA3OTAxLCJleHAiOjE2OTMwMTc1MDEsImF6cCI6IlRkSkljYmUxNldvVEh0Tjk1bnl5d2g1RTR5T282SXRHIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBtb2RlbC5yZWFkIG1vZGVsLnJlcXVlc3Qgb3JnYW5pemF0aW9uLnJlYWQgb3JnYW5pemF0aW9uLndyaXRlIG9mZmxpbmVfYWNjZXNzIn0.i1EnkatW-Ca78wKnSPdfMD29au4O4dz1adDRUNQmK3ghFOGprhOJIknWorNBKgDF1RdDWpLTKpkViIZdoGbrQkZNoKB7vi3BpnS76Pye1O3vP6hYJekPvKJkI67ZXEyUV2SAc09EUoPmZZIDdQWwIfun8dbjWQ5MPJJWFJVEcfWvkyCy330OSl9KRHvFqsJAGJgs_IK2Xm3DcfZV_a-V8T1YOoQ1ZkBi9X8N7lKfJs8PeYaWW88XM3psD41WwQvW4jvTan4fgRllWXBGFzwmLTpxlHxWWzLXObdYDiRwhAvqC5MPuF-y4mI7rgqh-y-aalO5l-DW0lV8MZkgzBFYUQ"
         })
-
         prompt = re.search(r'(gpt.*)', message.display).group(1).strip()
+        global response
         response = ""
-
-        for data in chatbot.ask(
-                prompt
-        ):
-            response = data["message"]
+        get_response(chatbot, prompt)
 
         await app.send_message(
             group,
-            MessageChain([Plain(response+f'\n\n本小时使用次数({used_count}/{LIMIT})')]),
+            MessageChain([Plain(response + f'\n\n本小时使用次数({used_count}/{LIMIT})')]),
         )
--- persistence/gpt_limit.py
+++ persistence/gpt_limit.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 
 def save_list(hour_list):
-    with open("./data/gpt_limit.json", "w") as file:
+    with open("./persistence/data/gpt_limit.json", "w") as file:
         json.dump(hour_list, file)
 
 
 def read_list():
-    with open("./data/gpt_limit.json", "r") as file:
+    with open("./persistence/data/gpt_limit.json", "r") as file:
         return json.load(file)
 
 
@@ -28,3 +28,7 @@ def is_under_limit(limit):
         hour_list[1] = 1
         save_list(hour_list)
         return [True, hour_list[1]]
+
+
+if __name__ == '__main__':
+    save_list([0, 0])
